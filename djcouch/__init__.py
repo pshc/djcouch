074bcf820968f145bb64fd46d4d606f57b35f22f
Paul Collier <paching@gmail.com>
2010-02-18 16:58:27 -0500

    Multi-DB support

--- djcouch/__init__.py
+++ djcouch/__init__.py
@@ -12,50 +12,50 @@ except AttributeError:
     raise CouchDBImproperlyConfigured("Please ensure that COUCHDB_HOST is "
             "set in your settings file.")
 
-def _determine_db_name():
-    id = getattr(settings, 'COUCHDB_DATABASE_NAME', None)
-    if not id:
-        id = getattr(settings, 'SITE_PREFIX', None)
-        if not id:
-            raise CouchDBImproperlyConfigured("Couldn't determine a default "
-                    "database name; please set COUCHDB_DATABASE_NAME "
-                    "in your settings.")
-    return id
+try:
+    DATABASE_NAMES = settings.COUCHDB_DATABASES
+except AttributeError:
+    raise CouchDBImproperlyConfigured("Please ensure that "
+            "COUCHDB_DATABASES is set in your settings file.")
+if not isinstance(DATABASE_NAMES, (list, tuple)):
+    raise CouchDBImproperlyConfigured("COUCHDB_DATABASES must be a list "
+            "of database names.")
 
-DATABASE_NAME = _determine_db_name()
-DESIGN_DOCNAME = getattr(settings, 'COUCHDB_DESIGN_DOCNAME',
-                         DATABASE_NAME + '-design')
+DEFAULT_DATABASE = getattr(settings, 'COUCHDB_DEFAULT_DATABASE', None)
+
+DESIGN_DOC_SUFFIX = getattr(settings, 'COUCHDB_DESIGN_DOC_SUFFIX', '-design')
 
 if not hasattr(settings, 'couchdb_server'):
     settings.couchdb_server = couchdb.client.Server(HOST)
 server = settings.couchdb_server
 
-if not hasattr(settings, 'couchdb_db'):
-    try:
-        settings.couchdb_db = server.create(DATABASE_NAME)
-    except PreconditionFailed as e:
-        if 'file_exists' not in str(e):
-            raise
-        settings.couchdb_db = server[DATABASE_NAME]
-db = settings.couchdb_db
+if not hasattr(settings, 'couchdb_dbs'):
+    settings.couchdb_dbs = {}
+    for _name in DATABASE_NAMES:
+        try:
+            _db = server.create(_name)
+        except PreconditionFailed as _e:
+            if 'file_exists' not in str(_e):
+                raise
+            _db = server[_name]
+        settings.couchdb_dbs[_name] = _db
+dbs = settings.couchdb_dbs
 
-def view(name, empty_ok=True, **kwargs):
+def view(view_name, db=None, empty_ok=True, **kwargs):
     """Wraps db.view(...) with convenience functionality.
-
-    Prepends $DESIGN_DOCNAME/ to the given view name.
-    If Django view foo() has a docstring containing a CouchDB view bar,
-    you should pass "foo.bar" as the view name.
 
     If keyword argument empty_ok=True (the default), swallows the
     ('not_found', 'missing') exception if no results are returned.
     """
-    name = '%s/%s' % (DESIGN_DOCNAME, name)
+    db_name = DEFAULT_DATABASE if db is None else db
+    db = dbs[db_name]
+    name = '%s%s/%s' % (db_name, DESIGN_DOC_SUFFIX, view_name)
     if empty_ok:
-        return _view_empty_ok(name, **kwargs)
+        return _view_empty_ok(db, name, kwargs)
     return db.view(name, **kwargs)
 
 # Stupid hack
-def _view_empty_ok(name, **kwargs):
+def _view_empty_ok(db, name, kwargs):
     try:
         for obj in db.view(name, **kwargs):
             yield obj
@@ -65,10 +65,10 @@ def _view_empty_ok(name, **kwargs):
         else:
             raise
 
-def get_document_or_404(id):
+def get_document_or_404(id, db=None):
     """Like get_object_or_404; raises Http404 if the document doesn't exist."""
     try:
-        return db[id]
+        return dbs[DEFAULT_DATABASE if db is None else db][id]
     except ResourceNotFound:
         raise Http404
 
--- djcouch/management.py
+++ djcouch/management.py
@@ -6,22 +6,28 @@ import re
 from textwrap import dedent
 
 section_division_re = re.compile(r'^\s*/////+\s*$', re.M)
-view_naming_re = re.compile(r'^(\w+)\s*=\s*(function.+)$', re.S)
+view_naming_re = re.compile(r'^(\w+\.)?(\w+)\s*=\s*(function.+)$', re.S)
 
 def parse_couchdb_views(func_name, doc, views, verbosity):
     for section in section_division_re.split(doc):
         m = view_naming_re.match(section.strip())
-        if m is not None:
-            name, code = m.groups()
-            name = '%s.%s' % (func_name, name)
-            views.append(ViewDefinition(djcouch.DESIGN_DOCNAME, name, code))
-            if verbosity:
-                print 'Found view %s' % (name,)
+        if not m:
+            continue
+        db_name, view_name, code = m.groups()
+        db_name = djcouch.DEFAULT_DATABASE if not db_name else db_name[:-1]
+        assert db_name in djcouch.DATABASE_NAMES, ("No such CouchDB "
+                "database '%s' in function %s" % (db_name, func_name))
+        design_doc = db_name + djcouch.DESIGN_DOC_SUFFIX
+        view_def = ViewDefinition(design_doc, view_name, code.strip())
+        db_views = views.setdefault(db_name, [])
+        db_views.append(view_def)
+        if verbosity:
+            print 'Found view %s.%s in %s' % (db_name, view_name, func_name)
 
 def create_couchdb_views(app, created_models, verbosity, **kwargs):
     """Searches each installed app's views.py for CouchDB
     view functions in view functions' docstrings."""
-    views = []
+    views = {}
     if verbosity:
         print 'Searching for CouchDB views...'
     for app_name in settings.INSTALLED_APPS:
@@ -41,7 +47,8 @@ def create_couchdb_views(app, created_models, verbosity, **kwargs):
     if views:
         if verbosity:
             print 'Synching CouchDB views.'
-        ViewDefinition.sync_many(djcouch.db, views)
+        for db_name, db_views in views.iteritems():
+            ViewDefinition.sync_many(djcouch.dbs[db_name], db_views)
     elif verbosity:
         print 'No CouchDB views found.'
 
